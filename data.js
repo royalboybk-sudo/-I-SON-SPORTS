5137fb21d0d60cbe7f6f3246eeb6e17def332930
royalboybk-sudo <royalboybk@gmail.com>
2026-02-20 11:27:49 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+let products = [
+  {
+    id: 1,
+    name: "Cricket Bat",
+    price: 1200,
+    category: "cricket",
+    image: "images/products/bat.jpg.webp",
+    active: true
+  },
+  {
+    id: 2,
+    name: "Cricket Gloves",
+    price: 450,
+    category: "cricket",
+    image: "images/products/gloves.jpj.jpg",
+    active: true
+  }
+];
